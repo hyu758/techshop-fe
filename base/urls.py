8f95ddbd4436895f79dab60509d53d2785cca80f
Ducchan <duc26503@gmail.com>
2024-09-15 17:38:21 +0700

    update cart + gitignore

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('profile/', profile, name = 'profile'),
     path('', home, name='home'),
     path('shop/', shop, name='shop'),
+    path('cart/', cart, name='cart'),
+    path('checkout/', checkout, name='checkout'),
     path('product/<int:product_id>/', product_detail, name='product_detail'),
+    path('api/add-to-cart/', add_to_cart, name='add_to_cart'),
     path('upload_image/', upload_image, name = "upload_image")
 ]
